9055a2aeeb90d564fd0f35eba0236f96304d5636
mudathirmahgoub <mudathirmahgoub@gmail.com>
2022-06-24 14:31:33 -0500

    Fix jar issues

--- SMT-Extension/src/main/java/edu/uiowa/alloy2smt/mapping/Mapper.java
+++ SMT-Extension/src/main/java/edu/uiowa/alloy2smt/mapping/Mapper.java
@@ -11,43 +11,43 @@ package edu.uiowa.alloy2smt.mapping;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
+//import javax.xml.bind.JAXBContext;
+//import javax.xml.bind.JAXBException;
+//import javax.xml.bind.Marshaller;
+//import javax.xml.bind.Unmarshaller;
+//import javax.xml.bind.annotation.XmlAttribute;
+//import javax.xml.bind.annotation.XmlRootElement;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@XmlRootElement(name = "Mapper")
+//@XmlRootElement(name = "Mapper")
 public class Mapper
 {
-  @XmlAttribute(name = "Signature")
+  // @XmlAttribute(name = "Signature")
   @JsonProperty("signatures")
   public List<MappingSignature> signatures = new ArrayList<>();
 
-  @XmlAttribute(name = "Field")
+  // @XmlAttribute(name = "Field")
   @JsonProperty("fields")
   public List<MappingField> fields = new ArrayList<>();
 
-  public void writeToXml(String xmlFile) throws JAXBException
-  {
-    JAXBContext context = JAXBContext.newInstance(Mapper.class);
-    Marshaller marshaller = context.createMarshaller();
-    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-    marshaller.marshal(this, new File(xmlFile));
-  }
+//  public void writeToXml(String xmlFile) throws JAXBException
+//  {
+//    JAXBContext context = JAXBContext.newInstance(Mapper.class);
+//    Marshaller marshaller = context.createMarshaller();
+//    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+//    marshaller.marshal(this, new File(xmlFile));
+//  }
 
-  public static Mapper readFromXml(String xmlFile) throws JAXBException
-  {
-    JAXBContext context = JAXBContext.newInstance(Mapper.class);
-    Unmarshaller unmarshaller = context.createUnmarshaller();
-    Mapper mapper = (Mapper) unmarshaller.unmarshal(new File(xmlFile));
-    return mapper;
-  }
+//  public static Mapper readFromXml(String xmlFile) throws JAXBException
+//  {
+//    JAXBContext context = JAXBContext.newInstance(Mapper.class);
+//    Unmarshaller unmarshaller = context.createUnmarshaller();
+//    Mapper mapper = (Mapper) unmarshaller.unmarshal(new File(xmlFile));
+//    return mapper;
+//  }
 
   public void writeToJson(String jsonFile) throws IOException
   {
